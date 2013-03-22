8175cadd443532f725b46e02e32a045823dd200b
root <root@liubin-ThinkPad-R61>
2013-03-22 13:38:26 +0800

    @sdlib

--- src/gethostbyname.c
+++ src/gethostbyname.c
@@ -18,12 +18,21 @@
 
 int main(int argc, char *argv[]) {
 	int opt;
-	char *host_c, **pptr;
+	char *host_c, **pptr,;
 	bool showversion = FALSE;
 	bool havehostarg = FALSE;
-	bool havelocalarg=FALSE;
+	bool havelocalarg = FALSE;
+	bool haveportarg = FALSE;
 	char str[100];
-	struct hostent *host_t;
+	struct hostent *host_s;
+
+	/*
+	 * v For version
+	 * l For localhost port
+	 * h For remote ip
+	 * p For remote port
+	 */
+
 	while ((opt = getopt(argc, argv, "vl:h:p:")) != -1) {
 		switch (opt) {
 		case 'v':
@@ -34,7 +43,11 @@ int main(int argc, char *argv[]) {
 			host_c = optarg;
 			break;
 		case 'l':
-			havelocalarg=TRUE;
+			havelocalarg = TRUE;
+			break;
+		case 'p':
+			haveportarg = TRUE;
+			break;
 		default:
 			break;
 		}
@@ -45,17 +58,17 @@ int main(int argc, char *argv[]) {
 	}
 
 	if (havehostarg) {
-		if ((host_t = gethostbyname(host_c)) == NULL ) {
+		if ((host_s = gethostbyname(host_c)) == NULL ) {
 			printf("gethostbyname error %d--%s\n", errno, strerror(errno));
 		}
 	}
 
-	printf("%s\n", host_t->h_name);
-	switch (host_t->h_addrtype) {
+	printf("%s\n", host_s->h_name);
+	switch (host_s->h_addrtype) {
 	case AF_INET:
 	case AF_INET6:
-		for (pptr = host_t->h_addr_list; *pptr != NULL ; pptr++) {
-			inet_pton(host_t->h_addrtype, *pptr, str, sizeof(str));
+		for (pptr = host_s->h_addr_list; *pptr != NULL ; pptr++) {
+			inet_pton(host_s->h_addrtype, *pptr, str, sizeof(str));
 			printf("%s\n", str);
 		}
 		break;
@@ -63,7 +76,7 @@ int main(int argc, char *argv[]) {
 		printf("unkown proto");
 	}
 
-	for (pptr = host_t->h_aliases; *pptr != NULL ; pptr++) {
+	for (pptr = host_s->h_aliases; *pptr != NULL ; pptr++) {
 		printf("%s\n", *pptr);
 	}
 
