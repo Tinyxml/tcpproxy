52753ed1ee3589a19629dc065d1ab03714f52b1a
root <root@liubin-ThinkPad-R61>
2013-03-22 16:09:33 +0800

    First Version that can run.But some process is zombie.Maybe i must process sigchild.

--- src/tcpproxy.c
+++ src/tcpproxy.c
@@ -1,8 +1,6 @@
 /*
- * tcpproxy.c
- *
- * Author: lvbingc <lvbingc@gmail.com>
- *
+ *  tcpproxy.c
+ *  Author: lvbingc <lvbingc@gmail.com>
  */
 
 #include<netdb.h>
@@ -11,10 +9,14 @@
 #include<unistd.h>
 #include<string.h>
 #include<errno.h>
+#include<sys/socket.h>
+#include<sys/types.h>
 
 #define bool int
 #define TRUE 1
 #define FALSE 0
+#define OK 1
+#define BUF_SIZE 1024
 
 int remote_port, local_port;
 int listen_fd;
@@ -22,6 +24,11 @@ char *remote_host;
 struct sockaddr_in server_addr;
 
 int create_server(int local_port);
+int server_loop();
+int handler_client(int connfd);
+int create_conn_server(char* remote_host, int remote_port);
+int redirect_data(int srcfd, int dstfd);
+
 int main(int argc, char *argv[]) {
 	int opt;
 	char **pptr;
@@ -67,48 +74,100 @@ int main(int argc, char *argv[]) {
 
 	if (!(local_port_flag && remote_port_flag && remote_port_flag)) {
 		printf("Usage: tcpproxy -l 8080 -h 127.0.0.1 -p 80\n");
+		exit(0);
 	} else {
 		printf("tcpproxy -l %d -h %s -p %d\n", local_port, remote_host,
 				remote_port);
 	}
 
-	create_server(local_port);
-	create_client(remote_host, remote_port);
-
-//
-//	if (remote_host_flag) {
-//		if ((host_s = gethostbyname(remote_host)) == NULL ) {
-//			printf("gethostbyname error %d--%s\n", errno, strerror(errno));
-//		}
-//	}
-//
-//	printf("%s\n", host_s->h_name);
-//	switch (host_s->h_addrtype) {
-//	case AF_INET:
-//	case AF_INET6:
-//		for (pptr = host_s->h_addr_list; *pptr != NULL ; pptr++) {
-//			inet_pton(host_s->h_addrtype, *pptr, str, sizeof(str));
-//			printf("%s\n", str);
-//		}
-//		break;
-//	default:
-//		printf("unkown proto");
-//	}
-//
-//	for (pptr = host_s->h_aliases; *pptr != NULL ; pptr++) {
-//		printf("%s\n", *pptr);
-//	}
-
+	if (create_server(local_port) == -1) {
+		printf("create server() error.Exiting...");
+		exit(-1);
+	}
+	server_loop();
 	return 0;
 }
 
 int create_server(int local_port) {
 	listen_fd = socket(AF_INET, SOCK_STREAM, 0);
-	if (listen_fd = -1) {
-		printf("socket() error %d : %s", errno, strerror(errno));
+	if (listen_fd == -1) {
+		printf("socket() error %d : %s\n", errno, strerror(errno));
+		return -1;
 	}
-	server_addr.sin_family = AF_INET;
 	server_addr.sin_port = htons(local_port);
-	server_addr.sin_addr.s_addr=htonl(ANYADDR);
+	server_addr.sin_addr.s_addr = htonl(INADDR_ANY );
+	if ((bind(listen_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)))
+			== -1) {
+		printf("bind() error %d : %s \n", errno, strerror(errno));
+		return -1;
+	}
+	if ((listen(listen_fd, 20)) == -1) {
+		printf("listen() error %d : %s \n", errno, strerror(errno));
+		return -1;
+	}
+	return listen_fd;
 }
 
+int server_loop() {
+	int connfd;
+	while (1) {
+		connfd = accept(listen_fd, (struct sockaddr *) NULL, NULL );
+		if (connfd == -1) {
+			printf("accept() error %d : %s\n", errno, strerror(errno));
+			return -1;
+		}
+		if (fork() == 0) {
+			close(listen_fd);
+			handler_client(connfd);
+			exit(0);
+		}
+		close(connfd);
+	}
+	return OK;
+}
+
+int handler_client(int connfd) {
+	int client_fd;
+	client_fd = create_conn_server(remote_host, remote_port);
+	if (fork() == 0) {
+		redirect_data(connfd, client_fd);
+		exit(0);
+	}
+	if (fork() == 0) {
+		redirect_data(client_fd, connfd);
+		exit(0);
+	}
+	close(connfd);
+	close(client_fd);
+	return OK;
+}
+
+int create_conn_server(char* remote_addr, int remote_port) {
+	int client_fd;
+	struct sockaddr_in client_addr;
+	client_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (client_fd == -1) {
+		printf("socket errno %d : %s \n", errno, strerror(errno));
+		return -1;
+	}
+	client_addr.sin_family = AF_INET;
+	client_addr.sin_port = htons(remote_port);
+	inet_pton(AF_INET, remote_addr, &client_addr.sin_addr);
+	if ((connect(client_fd, (struct sockaddr *) &client_addr,
+			sizeof(client_addr))) < 0) {
+		printf("connect error %d : %s \n", errno, strerror(errno));
+		return -1;
+	}
+	return client_fd;
+}
+
+int redirect_data(int srcfd, int dstfd) {
+	char buf[BUF_SIZE];
+	int n;
+	while ((n = recv(srcfd, buf, BUF_SIZE, 0)) > 0) {
+		send(dstfd, buf, n, 0);
+	}
+	close(srcfd);
+	close(dstfd);
+	return OK;
+}
